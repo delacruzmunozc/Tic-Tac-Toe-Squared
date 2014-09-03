bdd6550dd722148883da7eede48b602713a3fd70
Christian de la Cruz-Munoz <supermunch@live.com>
2014-09-03 13:13:40 -0400

    Multiplayer ready

--- app/src/main/java/com/supermunch/tictactoe/MyActivity.java
+++ app/src/main/java/com/supermunch/tictactoe/MyActivity.java
@@ -44,58 +44,6 @@ public class MyActivity extends Activity {
     {
         playMove(1);
     }
-    public void twoButtonClick(View view)
-    {
-        playMove(2);
-    }
-    public void threeButtonClick(View view)
-    {
-        playMove(3);
-    }
-    public void fourButtonClick(View view)
-    {
-        playMove(4);
-    }
-    public void fiveButtonClick(View view)
-    {
-        playMove(5);
-    }
-    public void sixButtonClick(View view)
-    {
-        playMove(6);
-    }
-    public void sevenButtonClick(View view)
-    {
-        playMove(7);
-    }
-    public void eightButtonClick(View view)
-    {
-        playMove(8);
-    }
-    public void nineButtonClick(View view)
-    {
-        playMove(9);
-    }
-    public void tenButtonClick(View view)
-    {
-        playMove(10);
-    }
-    public void elevenButtonClick(View view)
-    {
-        playMove(11);
-    }
-    public void twelveButtonClick(View view)
-    {
-        playMove(12);
-    }
-    public void thirteenButtonClick(View view)
-    {
-        playMove(13);
-    }
-    public void fourteenButtonClick(View view)
-    {
-        playMove(14);
-    }
     public void fifteenButtonClick(View view)
     {
         playMove(15);
